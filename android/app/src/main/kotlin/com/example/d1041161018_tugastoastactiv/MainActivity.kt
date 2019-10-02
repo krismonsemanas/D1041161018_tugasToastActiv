8ae313facc5c3a67de8a62f21a284e18fc9e2819
krismon setiawan <krismonsetiawan57@gmail.com>
2019-10-02 13:23:14 +0700

    Flutter - D1041161018_tugasToastActiv

---
+++ android/app/src/main/kotlin/com/example/d1041161018_tugastoastactiv/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.d1041161018_tugastoastactiv
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
